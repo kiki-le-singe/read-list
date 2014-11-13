a68b2e5866833662e4536b95a66c48914d92adb7
Anthony Albertini <kiki@MacBook-Pro-de-Anthony.local>
2014-11-14 00:21:05 +0100

    feat(helpers): add "is" helpers

--- app/scripts/helpers/helpers.js
+++ app/scripts/helpers/helpers.js
@@ -5,4 +5,7 @@ define([
 function (Handlebars) {
   'use strict';
 
+  Handlebars.registerHelper('is', function(value, test, options) {
+    return (value && value === test) ? options.fn(this) : options.inverse(this);
+  });
 });
