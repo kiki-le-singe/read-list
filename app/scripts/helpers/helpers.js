b596508f59fa8424e97bab01ed0bf7bcee940c61
Anthony Albertini <kiki@MacBook-Pro-de-Anthony.local>
2014-11-14 00:20:49 +0100

    chore(helpers): remove useless helpers

--- app/scripts/helpers/helpers.js
+++ app/scripts/helpers/helpers.js
@@ -5,14 +5,4 @@ define([
 function (Handlebars) {
   'use strict';
 
-  var bar = function (context, options) {
-    return context;
-  };
-
-  var foo = function (context, options) {
-    return context;
-  };
-
-  Handlebars.registerHelper('bar', bar);
-  Handlebars.registerHelper('foo', foo);
 });
